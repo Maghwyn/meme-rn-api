84b67eb78d554513839f752f9ef52e5ffe942ae9
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2024-03-07 04:18:21 +0100

    feat(meme-dto): Define the dto validation

--- src/memes/types/memes.type.ts
+++ src/memes/types/memes.type.ts
@@ -2,4 +2,29 @@ import { ObjectId } from 'mongodb';
 
 export type Meme = {
 	_id?: ObjectId;
+	userId: ObjectId;
+	title: string;
+	username: string;
+	category: string;
+	uploads: Array<MemeUpload>;
+	comments: Array<Comment>;
+	updatedAt: Date;
+	createdAt: Date;
+};
+
+export type MemeUpload = {
+	name: string;
+	size: number;
+	url: string;
+};
+
+export type Comment = {
+	username: string;
+	content: string;
+	createdAt: Date;
+};
+
+export type MemeSearchQuery = {
+	q?: string; // search query
+	c?: string; // category
 };
