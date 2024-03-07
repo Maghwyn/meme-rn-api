e3d780ee84488296216f51b01193a24704a31df7
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2024-03-07 03:40:31 +0100

    fix(uploads): Register the uploads module

--- src/uploads/uploads.module.ts
+++ src/uploads/uploads.module.ts
@@ -7,7 +7,7 @@ import { DatabaseModule } from '@/database/database.module';
 import { UsersModule } from '@/users/users.module';
 
 @Module({
-	imports: [DatabaseModule.forRoot(), UsersModule],
+	imports: [DatabaseModule.forRoot()],
 	providers: [UploadsService, UploadsRepository],
 	controllers: [UploadsController],
 })
