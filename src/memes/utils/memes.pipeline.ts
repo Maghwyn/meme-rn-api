8f5e5ed567d138a2527dd750173ad910618413d2
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2024-03-07 19:44:44 +0100

    fix(meme): Fix the memes comment pipeline

--- src/memes/utils/memes.pipeline.ts
+++ src/memes/utils/memes.pipeline.ts
@@ -1,52 +1,50 @@
 import { Document, ObjectId } from 'mongodb';
 
 export const getMemesCommentsPipeline = (userId: ObjectId): Array<Document> => [
-	[
-		{
-			$project: {
-				userId: 1,
-				title: 1,
-				username: 1,
-				category: 1,
-				upload: 1,
-				likes: 1,
-				comments: {
-					$filter: {
-						input: '$comments',
-						as: 'comment',
-						cond: {
-							$eq: ['$$comment.userId', userId],
-						},
+	{
+		$project: {
+			userId: 1,
+			title: 1,
+			username: 1,
+			category: 1,
+			upload: 1,
+			likes: 1,
+			comments: {
+				$filter: {
+					input: '$comments',
+					as: 'comment',
+					cond: {
+						$eq: ['$$comment.userId', userId],
 					},
 				},
 			},
 		},
-		{
-			$match: {
-				comments: {
-					$ne: [],
-				},
+	},
+	{
+		$match: {
+			comments: {
+				$ne: [],
 			},
 		},
-		{
-			$project: {
-				id: '$_id',
-				_id: 0,
-				userId: 1,
-				title: 1,
-				username: 1,
-				category: 1,
-				comments: 1,
-				pictureUrl: '$upload.url',
-				commentsCount: {
-					$size: '$comments',
-				},
-				likesCount: {
-					$size: '$likes',
-				},
+	},
+	{
+		$project: {
+			id: '$_id',
+			_id: 0,
+			userId: 1,
+			title: 1,
+			username: 1,
+			category: 1,
+			comments: 1,
+			pictureUrl: '$upload.url',
+			commentsCount: {
+				$size: '$comments',
+			},
+			likesCount: {
+				$size: '$likes',
 			},
 		},
-	],
+	},
 ];
 
 export const getMemesLikesPipeline = (userId: ObjectId): Array<Document> => [
