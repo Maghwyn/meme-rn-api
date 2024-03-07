cd95c678d2ceecdb41cf329c9e87ababaf0f26f7
Samson Christopher <91219719+Maghwyn@users.noreply.github.com>
2024-03-07 18:32:30 +0100

    feat(meme-mongodb): Create some pipeline to retrieve the comments and the linkes where the userId is present

--- src/memes/utils/memes.projection.ts
+++ src/memes/utils/memes.projection.ts
@@ -2,12 +2,32 @@ export const MEMES_PROJECTION = {
 	projection: {
 		id: '$_id',
 		_id: 0,
+		userId: 1,
 		title: 1,
 		username: 1,
-		uploads: 1,
 		category: 1,
+		upload: 1,
 		comments: 1,
+		likes: 1,
 		updatedAt: 1,
 		createdAt: 1,
+	},
+};
+
+export const MEMES_CREATED_PROJECTION = {
+	projection: {
+		id: '$_id',
+		_id: 0,
+		userId: 1,
+		title: 1,
+		username: 1,
+		category: 1,
+		pictureUrl: '$upload.url',
+		commentsCount: {
+			$size: '$comments',
+		},
+		likesCount: {
+			$size: '$likes',
+		},
 	},
 };
